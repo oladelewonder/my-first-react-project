9b5781579f448abe5496a4c2a9f0cbafd689e69c
wonder <wonderoladele@gmail.com>
2023-01-27 02:37:39 +0000

    trying out react

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <div>
+            <p>github logo</p>
+            <p>twitter logo</p>
+            <p>facebook logo</p>
+            <p>instagram logo</p>
+        </div>
+    )
+}
---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Info(){
+    return(
+       <div>
+        <img src="../img/wonder.jpeg"></img>
+        <h1>Oladele wonder</h1>
+        <h3>Frontend developer</h3>
+        <h4>oladelewonder.website</h4>
+        <div>
+            <button>email</button>
+            <button>linkedin</button>
+        </div>
+       </div> 
+    )
+}
